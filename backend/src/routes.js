9bd7cdf4f5df010e4daef33c4a0fe2a1c8a02475
Paulo Henrique <phenriq694@gmail.com>
2019-10-03 12:32:49 -0300

    Configuração inicial do arquivo 'routes' e exportação deste módulo para o arquivo server.js

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const routes = express.Router()
+
+// Métodos do Express: GET, POST, PUT, DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+/* 
+ * Utilizando o método GET do Express para pegar o endereço digitado no navegador e 
+ * fazer algo com essa informação passando uma função (arrow function) com os parâmetros 'res' de response e 'req' de request, 
+ * para tratar as requisições e responder para client. 
+ */
+routes.post('/users', (req, res) => {
+    return res.json({message : 'Hello World!'})
+})
+
+module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,15 @@
-/* Importação do módulo Express */
+// Importação do módulo Express 
 const express = require('express')
 
-/* Criação da aplicação */
+const routes = require('./routes')
+
+// Criação da aplicação 
 const app = express()
 
-/* 
- * Utilizando o método GET do Express para pegar o endereço digitado no navegador e 
- * fazer algo com essa informação passando uma função (arrow function) com os parâmetros 'res' de response e 'req' de request, 
- * para tratar as requisições e responder para client. 
- */
-app.get('/', (req, res) => {
-    return res.json({message : 'Hello World!'})
-})
+// Informando para a aplicação Express que as requisições estão utilizando o tipo JSON
+app.use(express.json())
+
+app.use(routes)
 
 /* Configurando a porta que a aplicação vai ouvir */
 app.listen(3000)

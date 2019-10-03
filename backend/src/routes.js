df01cf150193211ec8e4444a437a631f341bbe16
Paulo Henrique <phenriq694@gmail.com>
2019-10-03 16:26:54 -0300

    BACKEND: Criação do controller 'DashboardController.js' e configuração do método 'show' para exibição de todos os spots cadastrados no banco.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload')
 
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
 
 
 const routes = express.Router()
@@ -26,5 +27,6 @@ routes.post('/sessions', SessionController.store)
 routes.get('/spots', SpotController.index)
 routes.post('/spots', upload.single('thumbnail'),SpotController.store)
 
+routes.get('/dashboard', DashboardController.show)
 
 module.exports = routes

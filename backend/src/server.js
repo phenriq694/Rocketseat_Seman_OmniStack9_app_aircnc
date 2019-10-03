f632d9ead4e36b58d55381844115ee64b6f7e390
Paulo Henrique <phenriq694@gmail.com>
2019-10-03 12:47:30 -0300

    Configuração a conexão do MongoDB no arquivo server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
 // Importação do módulo Express 
 const express = require('express')
 
+// Importação do Mogoose para facilitar o trabalho ocm o MongoDB
+const mongoose = require('mongoose')
+
 const routes = require('./routes')
+
 
 // Criação da aplicação 
 const app = express()
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-byatw.mongodb.net/semana09?retryWrites=true&w=majority',
+{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
 
 // Informando para a aplicação Express que as requisições estão utilizando o tipo JSON
 app.use(express.json())

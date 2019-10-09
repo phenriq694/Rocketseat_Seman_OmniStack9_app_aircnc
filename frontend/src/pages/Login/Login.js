649ec97d2d10bea40eb335082ee41fa3d51e42f5
Paulo Henrique <phenriq694@gmail.com>
2019-10-09 13:02:19 -0300

    FRONTEND: Configuração da rota '/dashboard' 	- Acessando o backend e buscando as informações sobre os spots adicionados por um usuário. 	- Atualizando na tela todos os spots retornados do backend.

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import api from '../../services/api'
 
-export default function Login() {
+export default function Login({ history }) {
 
     const [email, setEmail] = useState('')
 
@@ -13,25 +13,27 @@ export default function Login() {
       const { _id } = response.data
   
       localStorage.setItem('user', _id)
+
+      history.push('/dashboard')
     }
 
     return (
         <>
             <p>
-            Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
+                Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
             </p>
 
             <form onSubmit={handleSubmit}>
-            <label htmlFor="email">E-MAIL *</label>
-            <input
-                id="email"
-                type="email"
-                placeholder="Seu melhor e-mail"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-            />
+                <label htmlFor="email">E-MAIL *</label>
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="Seu melhor e-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
 
-            <button className="btn" type="submit">Entrar</button>
+                <button className="btn" type="submit">Entrar</button>
             </form>
         </>
     )
